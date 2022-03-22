3d6868f59b0f83f54314524151c219edb3c79982
Babak <babak.jahanbani@yahoo.com>
2022-03-22 22:13:42 +0430

    CardForm + FormGroup + FormControl

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardFormComponent } from './card-form/card-form.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, CardFormComponent],
   imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
